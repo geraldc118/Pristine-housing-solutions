1a242d80ae652878a3df0d61b359aef99acc42ac
Gerald <geraldcameron118@gmail.com>
2024-10-11 16:51:14 +0100

    updates

--- src/Js/Contact.js
+++ src/Js/Contact.js
@@ -5,21 +5,21 @@ function Contact(){
     return(
         <div className="container my-5">
             <h1>Contact Us</h1>
-            <p>If you are interested in letting us use your property or alternatively you are looking for a property fill the form and we will get back to you.</p>
+            <p>If you are interested in letting us use your property or alternatively you are looking htmlfor a property fill the form and we will get back to you.</p>
             <form action="https://formspree.io/f/myyrrlgq" method="POST">
             <div className="form-group">
-                <label for="name">Name</label>
+                <label htmlfor="name">Name</label>
                 <input type="text" className="form-control" id="name" name="name" placeholder="Enter your name"/>
             </div>
             <div className="form-group">
-                <label for="email">Email address</label>
+                <label htmlfor="email">Email address</label>
                 <input type="email" className="form-control" id="email" name="email" placeholder="Enter your email"/>
             </div>
             <div className="form-group">
-                <label for="message">Message</label>
+                <label htmlfor="message">Message</label>
                 <textarea className="form-control" id="message" name="message" rows="3" placeholder="Enter your message"></textarea>
             </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" className="contactButton btn btn-primary">Submit</button>
             </form>
         </div>
     )
