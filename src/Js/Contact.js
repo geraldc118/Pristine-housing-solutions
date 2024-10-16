d56aa9ccb5970f3d2bc09d05e712701ef54382f5
Gerald <geraldcameron118@gmail.com>
2024-10-16 15:56:33 +0100

    updating images on pages

--- src/Js/Contact.js
+++ src/Js/Contact.js
@@ -1,25 +1,33 @@
 import React from "react";
 import '../Css/Contact.css';
+import logoImage from '../Images/android-chrome-512x512.png';
 
 function Contact(){
     return(
         <div className="container my-5">
+            <div className="row">
+                <div className="col-12">
+                    <div className="logoContainer">
+                        <img className="logoImage" src={logoImage} alt="logo"/>
+                    </div>
+                </div>
+           </div>
             <h1>Contact Us</h1>
-            <p>If you are interested in letting us use your property or alternatively you are looking htmlfor a property fill the form and we will get back to you.</p>
+            <p>If you are interested in letting us use your property or alternatively you are looking for a property fill the form and we will get back to you.</p>
             <form action="https://formspree.io/f/myyrrlgq" method="POST">
             <div className="form-group">
-                <label htmlfor="name">Name</label>
+                <label htmlFor="name">Name</label>
                 <input type="text" className="form-control" id="name" name="name" placeholder="Enter your name"/>
             </div>
             <div className="form-group">
-                <label htmlfor="email">Email address</label>
+                <label htmlFor="email">Email address</label>
                 <input type="email" className="form-control" id="email" name="email" placeholder="Enter your email"/>
             </div>
             <div className="form-group">
-                <label htmlfor="message">Message</label>
+                <label htmlFor="message">Message</label>
                 <textarea className="form-control" id="message" name="message" rows="3" placeholder="Enter your message"></textarea>
             </div>
-            <button type="submit" className="contactButton btn btn-primary">Submit</button>
+                <button type="submit" className="contactButton btn">Submit</button>
             </form>
         </div>
     )
--- src/Js/Services.js
+++ src/Js/Services.js
@@ -1,7 +1,6 @@
 import React from "react";
 import '../Css/Services.css';
 
-
 function Services(){
     return(
         <div className="container my-5">
@@ -9,17 +8,17 @@ function Services(){
             <div className="col-md-4">
               <h2>Our Services</h2>
               <p>We provide professional property management services for HMO and short stays.</p>
-              <a href="/about" className="serviceButton btn btn-primary">Learn More</a>
+              <a href="/about" className="serviceButton btn">Learn More</a>
             </div>
             <div className="col-md-4">
               <h2>Why Choose Us?</h2>
               <p>We ensure your property is well-maintained and occupied by reliable professionals.</p>
-              <a href="/about" className="serviceButton btn btn-primary">Learn More</a>
+              <a href="/about" className="serviceButton btn active">Learn More</a>
             </div>
             <div className="col-md-4">
               <h2>Contact Us</h2>
               <p>Get in touch with us to find out how we can manage your property.</p>
-              <a href="/contact" className="serviceButton btn btn-primary">Contact Us</a>
+              <a href="/contact" className="serviceButton btn active">Contact Us</a>
             </div>
           </div>
         </div>
